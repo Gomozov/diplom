46a5e659a57bb40d97c062d21f6c22836f6a3ad6
Gomozov <valeriy.gomozov@gmail.com>
2010-03-20 21:31:52 +0300

    Обработка широты и долготы + сортировка хэша

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -21,12 +21,15 @@ class DevicesController < ApplicationController
     @device = Device.find(params[:id])
     @report_id = Report.last(:conditions => {:device_id => @device.id})
     @report = ReportField.all(:conditions => {:report_id => @report_id.id})
+    @report = @report.sort_by { |t| t.key }
+    @lat =    ReportField.find(:first, :conditions => {:report_id => @report_id.id, :key => 'latitude'})
+    @lon =    ReportField.find(:first, :conditions => {:report_id => @report_id.id, :key => 'longitude'})
 
     gg = GoogleGeocode.new "ABQIAAAAeSgpsuI2BCtpNLyED8LDQBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxRnm97MQYcMTzXsEX4lf8tuo6XmWA"           
     @map = GMap.new("map_div")                                                                                                
     @map.control_init(:small => true, :large_map => true)                      
-    @map.center_zoom_init([56.3, 32],7)                                                                                        
-    @map.overlay_init(GMarker.new([56.3, 32],:title => @device.device_code, :info_bubble => 'ConnectPort X4'))
+    @map.center_zoom_init([@lat.value, @lon.value],7)                                                                                        
+    @map.overlay_init(GMarker.new([@lat.value, @lon.value],:title => @device.device_code, :info_bubble => 'ConnectPort X4'))
 
     respond_to do |format|
       format.html # show.html.erb
