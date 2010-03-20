f8fa61800839e0e108a19a41582036c128e2abf8
Victor Gumayunov <gumayunov@gmail.com>
2010-03-20 22:19:20 +0300

    report fixes

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,7 +1,5 @@
 class DevicesController < ApplicationController
   
-  require 'rubygems'
-  require 'google_geocode'
   
   # GET /devices
   # GET /devices.xml
@@ -19,17 +17,16 @@ class DevicesController < ApplicationController
   def show
 
     @device = Device.find(params[:id])
-    @report_id = Report.last(:conditions => {:device_id => @device.id})
-    @report = ReportField.all(:conditions => {:report_id => @report_id.id})
-    @report = @report.sort_by { |t| t.key }
-    @lat =    ReportField.find(:first, :conditions => {:report_id => @report_id.id, :key => 'latitude'})
-    @lon =    ReportField.find(:first, :conditions => {:report_id => @report_id.id, :key => 'longitude'})
+    @report = @device.report.last
+    @report_fields = @report.fields :order => 'key'
+    @lat =  @report['latitude']
+    @lon =  @report['longitude']
 
     gg = GoogleGeocode.new "ABQIAAAAeSgpsuI2BCtpNLyED8LDQBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxRnm97MQYcMTzXsEX4lf8tuo6XmWA"           
     @map = GMap.new("map_div")                                                                                                
     @map.control_init(:small => true, :large_map => true)                      
-    @map.center_zoom_init([@lat.value, @lon.value],7)                                                                                        
-    @map.overlay_init(GMarker.new([@lat.value, @lon.value],:title => @device.device_code, :info_bubble => 'ConnectPort X4'))
+    @map.center_zoom_init([@lat, @lon],7)                                                                                        
+    @map.overlay_init(GMarker.new([@lat, @lon],:title => @device.device_code, :info_bubble => 'ConnectPort X4'))
 
     respond_to do |format|
       format.html # show.html.erb
