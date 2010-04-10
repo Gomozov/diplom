ce7c0a77888858b49f3527c6cf49610831afc145
Victor Gumayunov <gumayunov@gmail.com>
2010-04-11 01:45:22 +0400

    инициализируем последний репорт при генерации страницы

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -12,14 +12,14 @@ class DevicesController < ApplicationController
   def show
 
     @device = Device.find params[:id]
-    @report = @device.reports.last
-    @fields = @report.fields.all :order => 'key'
+    @last_report = @device.reports.last
+    @fields = @last_report.fields.all :order => 'key'
     
     #gg = GoogleGeocode.new "ABQIAAAAeSgpsuI2BCtpNLyED8LDQBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxRnm97MQYcMTzXsEX4lf8tuo6XmWA"
     @map = GMap.new "map_div"
     @map.control_init :small => true, :large_map => true, :map_type => true
     
-    geo_point =  [ @report['latitude'], @report['longitude'] ]
+    geo_point =  [ @last_report['latitude'], @last_report['longitude'] ]
     @map.center_zoom_init geo_point, 6
 
     marker = GMarker.new geo_point, :title => @device.device_code, :info_window => '<b>ConnectPort X4</b>'
