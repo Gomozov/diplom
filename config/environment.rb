8f1d7b2ae1a827bb756b113f4c009c71821a95ac
Victor Gumayunov <gumayunov@gmail.com>
2010-06-26 23:45:53 +0400

    'devise' gem added into env

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   #config.gem "google_geocode"
   config.gem "google-geocode", :lib => "google_geocode"
+  config.gem "devise"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
