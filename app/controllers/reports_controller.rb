e50cad7f1ca5d04a2d182f89dd2bcbcb43effe28
Gomozov <Valeriy.gomozov@gmail.com>
2010-11-05 20:52:51 +0300

    убрал всплывающие подсказки в chart

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -45,13 +45,13 @@ class ReportsController < ApplicationController
   end
 
   def index
-   @reports = Report.find (:all, :conditions => {:device_id => params[:id]})
-   @keys = ReportField.find (:all, :conditions => {:key => 'Temp data WRouter', :report_id => @reports.map(&:id)})
+   @reports = Report.find(:all, :conditions => {:device_id => params[:id]})
+   @keys = ReportField.find(:all, :conditions => {:key => 'Light data WRouter', :report_id => @reports.map(&:id)})
    @values_m = []
    @report_id_m = []
    @keys.each do |d|
-     @values_m.push (d.value)
-     @report_id_m.push (d.report_id)
+     @values_m.push(d.value)
+     @report_id_m.push(d.report_id)
    end
   end
 end
